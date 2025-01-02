432d397e1de59e1a4e687c84ed5a5c9af6d1fabb
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2025-01-02 01:11:15 -0300

    Update const.cpp

--- POO/const.cpp
+++ POO/const.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+class Producto {
+private:
+    string nombre;
+    double precio;
+
+public:
+    Producto(string n, double p) : nombre(n), precio(p) {}
+
+    // Marcamos el metodo como const para permitir su uso en objetos constantes
+    void mostrar() const { 
+        cout << nombre << " cuesta " << precio << " unidades." << endl;
+    }
+};
+
+int main() {
+    const Producto p1("Producto 1", 10.5);  // Objeto constante
+    p1.mostrar();  // Esto ahora funciona porque mostrar es const
+
+    const Producto& p2 = p1;  // Referencia constante
+    p2.mostrar();  // Esto tambien funciona porque mostrar es const
+
+    return 0;
+}
