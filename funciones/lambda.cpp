771a292544d3c3639f3718c3087c905610dac656
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2025-01-02 01:33:28 -0300

    Update lambda.cpp

--- funciones/lambda.cpp
+++ funciones/lambda.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+int main() {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    std::for_each(vec.begin(), vec.end(), [](int n) {
+        std::cout << n * 2 << " ";
+    });
+    return 0;
+}
